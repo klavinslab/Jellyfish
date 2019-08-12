a0eb1c1342a09a8b5cbc53351fbb48b2afd21db8
Ben Keller <bjkeller@uw.edu>
2019-08-12 10:32:29 -0700

    Format resources file

--- default_resources.py
+++ default_resources.py
@@ -1,10 +1,10 @@
 resources = {
     "aquarium": {
-        "aquarium_url":{
-        	"Production": "http://52.27.43.242/",
-        	"Nursery": "http://52.27.43.242:81/",
-        	"Local": "http://localhost/"
-        }, 
+        "aquarium_url": {
+            "Production": "http://52.27.43.242/",
+            "Nursery": "http://52.27.43.242:81/",
+            "Local": "http://localhost/"
+        },
         "login": "Username",
         "password": "password"
     }
